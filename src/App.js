99dd9f13743f1ba03b99180a4a39fa8503858635
Fernando Lopez <fer.ragnar19@gmail.com>
2017-11-06 11:03:42 -0300

    agregado componentes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('/api/book')
+      .then(res => {
+        this.setState({ books: res.data });
+        console.log(this.state.books);
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div class="container">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">
+              CATALOGO DE LIBROS
+            </h3>
+          </div>
+          <div class="panel-body">
+            <h4><Link to="/create"><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> Añadir Libro</Link></h4>
+            <table class="table table-stripe">
+              <thead>
+                <tr>
+                  <th>ISBN</th>
+                  <th>Title</th>
+                  <th>Author</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.books.map(book =>
+                  <tr>
+                    <td><Link to={`/show/${book._id}`}>{book.isbn}</Link></td>
+                    <td>{book.title}</td>
+                    <td>{book.author}</td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     );
   }
