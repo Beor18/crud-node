43f9600ed2afd97b877fc04f82380b7d38cd507e
Fernando Lopez <fer.ragnar19@gmail.com>
2017-11-06 10:37:15 -0300

    actualizado

--- routes/book.js
+++ routes/book.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var Book = require('../models/Book.js');
 
-/* GET home page. */
+/* GET todos los libros */
 router.get('/', function(req, res, next) {
-  res.send('Express REST API');
+  Book.find(function (err, products) {
+    if (err) return next(err);
+    res.json(products);
+  });
+});
+
+/* GET un libro */
+router.get('/:id', function(req, res, next) {
+  Book.findById(req.params.id, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* Guardar Libro */
+router.post('/', function(req, res, next) {
+  Book.create(req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* Actualizar Libro */
+router.put('/:id', function(req, res, next) {
+  Book.findByIdAndUpdate(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
+});
+
+/* Eliminar Libro */
+router.delete('/:id', function(req, res, next) {
+  Book.findByIdAndRemove(req.params.id, req.body, function (err, post) {
+    if (err) return next(err);
+    res.json(post);
+  });
 });
 
 module.exports = router;
